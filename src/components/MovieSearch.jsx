b70448024e34c2232045504ce3bff73881882c88
SirCQQ <gatucristian@gmail.com>
2020-09-26 00:26:18 +0300

    Adding the template

---
+++ src/components/MovieSearch.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function MovieSearch() {
+    return (
+        <div>
+            <nav className="search-nav">
+                <form>
+                    <input 
+                    type="text"
+                    placeholder="Avengers"
+                    />
+                </form>
+            </nav>
+
+        </div>
+    )
+}
+
+export default MovieSearch
